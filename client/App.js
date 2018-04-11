2dccebe1c0dc1aaadf214f1720b644ed63cfd7eb
Vikram Belde <belvikram@gmail.com>
2018-04-10 20:23:50 -0500

    initial commit

---
+++ client/App.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react'// importing from node_modules
+class MyComponent extends Component {
+   render(){
+			return(
+			<div>
+			 	<h1> This is my first React Component </h1>
+			 	<h1> This is my first React Component </h1>
+			</div>
+		)
+	}
+}
+
+module.exports = MyComponent
